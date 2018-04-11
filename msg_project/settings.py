034602e2084b6a94eb9c9203ddde6e5a2cab4ab3
Raymond Rupiah <rupiray@yahoo.com>
2018-04-11 05:12:30 +0100

    implemented user authentication flow

--- msg_project/settings.py
+++ msg_project/settings.py
@@ -128,4 +128,13 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4h'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'Django-Man'
+EMAIL_HOST_PASSWORD = 'password123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
